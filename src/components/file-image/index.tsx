b248898300d9c045160861d7543ebabc30d9a3d0
unknown <shopkone>
2024-10-16 18:41:55 +0800

    feat

--- src/components/file-image/index.tsx
+++ src/components/file-image/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { IconPhotoX } from '@tabler/icons-react'
 import { useDebounce } from 'ahooks'
 import { Flex, Image } from 'antd'
@@ -7,7 +7,6 @@ import classNames from 'classnames'
 import { FileType } from '@/api/file/add-file-record'
 import PhotoX from '@/assets/icon/photo-x.svg'
 import SLoading from '@/components/s-loading'
-import SRender from '@/components/s-render'
 
 import styles from './index.module.less'
 
@@ -29,8 +28,6 @@ export interface FileImageProps {
 
 export default function FileImage (props: FileImageProps) {
   const { src, alt, width = 40, height = 40, type, loading = false, padding, style, className, size = 24, error, forceNoLoading, containerStyle } = props
-  const [imgLoading, setImgLoading] = useState(true)
-  const [init, setInit] = useState<string>()
 
   const temp = useMemo(() => {
     if (type === FileType.Image) {
@@ -44,15 +41,6 @@ export default function FileImage (props: FileImageProps) {
 
   const link = useDebounce(temp, { wait: 150 })
 
-  useEffect(() => {
-    if (!link || (link === init)) {
-      setImgLoading(false)
-    } else if (!imgLoading) {
-      setImgLoading(!forceNoLoading && true)
-    }
-    setInit(link)
-  }, [link])
-
   if (error) {
     return (
       <Flex style={{ width, height: height || width, ...containerStyle }} justify={'center'} align={'center'} className={styles.container}>
@@ -63,39 +51,25 @@ export default function FileImage (props: FileImageProps) {
 
   return (
     <div style={{ height: (height || width) }}>
-      <Flex align={'center'} justify={'center'} style={{ width, height: height || width, display: (loading || imgLoading) ? undefined : 'none' }}>
+      <Flex align={'center'} justify={'center'} style={{ width, height: height || width, display: loading ? undefined : 'none' }}>
         <SLoading loading size={size} />
       </Flex>
-      <div style={{ display: (loading || imgLoading) ? 'none' : undefined, ...containerStyle }} className={styles.container}>
-        <SRender render={init}>
-          <Image
-            onError={() => {
-              setImgLoading(false)
-            }}
-            onLoadCapture={() => {
-              setImgLoading(false)
-            }}
-            onLoad={() => {
-              setImgLoading(false)
-            }}
-            onLoadStart={() => {
-              setImgLoading(true)
-            }}
-            fallback={PhotoX as unknown as string}
-            preview={false}
-            alt={alt}
-            src={link}
-            width={width || style?.width}
-            height={height || style?.height}
-            className={classNames(styles.img, className)}
-            style={{
-              width,
-              height,
-              padding,
-              ...style
-            }}
-          />
-        </SRender>
+      <div style={{ display: loading ? 'none' : undefined, ...containerStyle }} className={styles.container}>
+        <Image
+          fallback={PhotoX as unknown as string}
+          preview={false}
+          alt={alt}
+          src={link}
+          width={width || style?.width}
+          height={height || style?.height}
+          className={classNames(styles.img, className)}
+          style={{
+            width,
+            height,
+            padding,
+            ...style
+          }}
+        />
       </div>
     </div>
   )
