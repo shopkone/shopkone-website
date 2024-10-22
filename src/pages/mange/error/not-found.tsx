e7412ce72c291be10bb9716d62ecde6468c00541
unknown <shopkone>
2024-10-22 21:36:32 +0800

    feat

--- src/pages/mange/error/not-found.tsx
+++ src/pages/mange/error/not-found.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 import { Button, Flex } from 'antd'
 
 import styles from './index.module.less'
@@ -10,14 +11,18 @@ export interface NotFoundProps {
 export default function NotFound (props: NotFoundProps) {
   const { t } = useTranslation('common', { keyPrefix: 'notFound' })
 
+  const nav = useNavigate()
+
   return (
     <Flex gap={24} vertical={props.veritcal} align={'center'} justify={'center'} style={{ height: '100%' }}>
       <div>asd</div>
       <div>
-        <div className={styles.title}>There's no page at this address</div>
+        <div className={styles.title}>{t('找不到页面')}</div>
         <Flex align={'center'} style={{ fontSize: 14 }}>
-          请检查网址并重试，或者你可以选择
-          <Button style={{ fontSize: 14, padding: 0 }} type={'link'} size={'small'}>{t('返回首页')}</Button>
+          {t('请检查网址并重试，或者你可以选择')}
+          <Button onClick={() => { nav('/') }} style={{ fontSize: 14, padding: 0 }} type={'link'} size={'small'}>
+            {t('回到首页')}
+          </Button>。
         </Flex>
       </div>
     </Flex>
