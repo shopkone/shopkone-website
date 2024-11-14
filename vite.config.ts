8077804431d34cfd28dc4d5491d7219851b4a3de
shopkone <sorder.co@gmail.com>
2024-11-14 16:44:49 +0800

    feat

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import { visualizer } from 'rollup-plugin-visualizer'
+import {defineConfig} from 'vite'
+import {visualizer} from 'rollup-plugin-visualizer'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from '@svgr/rollup'
+import {Plugin as importToCDN} from "vite-plugin-cdn-import"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,11 @@ export default defineConfig({
       filename: 'analysis-chart.html', // 分析图生成的文件名
       open: true // 如果存在本地服务端口，将在打包后自动展示
     }),
+    importToCDN({
+      modules:[
+        { name:"ali-oss", var:"OSS", path:"https://gosspublic.alicdn.com/aliyun-oss-sdk-6.18.1.min.js" }
+      ]
+    })
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') }
